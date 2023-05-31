c24135eb59fa340fed5741afb5ad202cb6c37ca5
Kent O'Sullivan <osullik@protonmail.com>
2023-05-31 17:30:15 -0400

    created unit test framework

--- Code/Graph_Problems/BFS/kent/bfs/test.c
+++ Code/Graph_Problems/BFS/kent/bfs/test.c
@@ -1,3 +1,40 @@
 // test.c (for testing bfs.c)
 
+//System imports
+#include <assert.h>
+#include <stdbool.h>
 
+//User defined imports
+#include "bfs.h"
+#include "bfs.c"
+
+//Test Functions
+
+static void test_bfs_single_vertex(){
+
+}
+
+static void test_bfs_single_vertex_with_loop(){
+
+}
+
+static void test_bfs_two_verticies_no_path(){
+
+}
+
+static void test_bfs_two_verticies_with_simple_path(){
+
+}
+
+static void test_bfs_complete_graph(){
+
+}
+
+static void test_bfs_cycle(){
+
+}
+
+//Main Function
+int main(){
+	assert(false && "My first unit test");
+}
