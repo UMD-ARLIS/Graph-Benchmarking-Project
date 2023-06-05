01e436bd539640fec2396b58640d9849693b8d75
osullik <osullik@protonmail.com>
2023-06-05 15:15:08 -0400

    implemented the MinUnit Test package and completed tests for stacks and queues

--- Code/Graph_Problems/BFS/kent/bfs/test.c
+++ Code/Graph_Problems/BFS/kent/bfs/test.c
@@ -8,8 +8,11 @@
 #include <string.h>
 
 //Util Imports
-# include "../../../../Utilities/c-utils/src/graph.h"
-# include "../../../../Utilities/sets/src/set.h"
+# include "../../../../Utilities/c-utils/src/minunit.h" //doesn't have a c file, so doesn't need to be invoked in make file.
+
+# include "../../../../Utilities/c-utils/src/graph.h" 	//invoke graph.c in makefile
+# include "../../../../Utilities/sets/src/set.h"		//invoke set.c in makefile
+# include "../../../../Utilities/c-utils/src/queue.h"	//invoke queue.c in makefile
 
 //User defined imports
 #include "bfs.h"
@@ -401,31 +404,188 @@ static void test_set_empty(){
 
 static void test_set_single_item(){
 
-}
+	printf("\n\n==== Test Single Item Set ====\n");
 
-static void test_set_two_items(){
+	//Init 
+	graph_t test_graph = g_init();
+	g_vertex_add(test_graph, "1"); //0
+	g_vertex_add(test_graph, "2"); //1
+	g_vertex_add(test_graph, "3"); //2
+	g_vertex_add(test_graph, "4"); //3
+	g_vertex_add(test_graph, "5"); //4
+	g_vertex_add(test_graph, "6"); //5
+	g_vertex_add(test_graph, "7"); //6
+	g_vertex_add(test_graph, "8"); //7
+	g_vertex_add(test_graph, "9"); //8
+
+	g_edge_add(test_graph, 0, 1, "A"); // (1,2)
+	g_edge_add(test_graph, 1, 2, "B"); // (2,3)
+	g_edge_add(test_graph, 1, 3, "C"); // (2,4)
+	g_edge_add(test_graph, 0, 4, "D"); // (1,5)
+	g_edge_add(test_graph, 4, 5, "E"); // (5,6)
+	g_edge_add(test_graph, 0, 6, "F"); // (1,7)
+	g_edge_add(test_graph, 6, 7, "G"); // (7,8)
+	g_edge_add(test_graph, 6, 8, "H"); // (7,9)
+
+    // Test
+
+    printf("Single item string set should have length of 1: ");
+    SimpleSet stringset;
+    set_init(&stringset);
+
+    set_add(&stringset, "test");
+
+	int setLength = set_length(&stringset);
+
+    if (setLength == 1){
+    	success_or_failure(1);
+    }
+    else{
+    	success_or_failure(0);
+    }    
+
+
+
+    printf("Single item graph node set should have length of 1: ");
+    SimpleSet graphset;
+    set_init(&graphset);
+
+    vertex_t nodeToAdd = g_vertex_get(test_graph, 0);
+
+    set_add(&graphset, nodeToAdd);
+
+	int graphSetLength = set_length(&graphset);
+
+    if (graphSetLength == 1){
+    	success_or_failure(1);
+    }
+    else{
+    	success_or_failure(0);
+    }    
+
+    printf("The item in the set is actually a graph node: ");
+
+    vertex_t candidateNode = g_vertex_get(test_graph, 0);
+
+    if (set_contains(&graphset, candidateNode) == SET_TRUE) {
+    	success_or_failure(1);
+    } else {
+    	success_or_failure(0);
+    }
+
+    // Cleanup
+    set_destroy(&stringset);
+    set_destroy(&graphset);
+
+   	g_free_alt(test_graph, false);
 
 }
 
 static void test_set_no_duplicates_added(){
 
+	printf("\n\n==== Test Deduplication ====\n");
+
+
+	//Init 
+	graph_t test_graph = g_init();
+	g_vertex_add(test_graph, "1"); //0
+	g_vertex_add(test_graph, "2"); //1
+	g_vertex_add(test_graph, "3"); //2
+	g_vertex_add(test_graph, "4"); //3
+	g_vertex_add(test_graph, "5"); //4
+	g_vertex_add(test_graph, "6"); //5
+	g_vertex_add(test_graph, "7"); //6
+	g_vertex_add(test_graph, "8"); //7
+	g_vertex_add(test_graph, "9"); //8
+
+	g_edge_add(test_graph, 0, 1, "A"); // (1,2)
+	g_edge_add(test_graph, 1, 2, "B"); // (2,3)
+	g_edge_add(test_graph, 1, 3, "C"); // (2,4)
+	g_edge_add(test_graph, 0, 4, "D"); // (1,5)
+	g_edge_add(test_graph, 4, 5, "E"); // (5,6)
+	g_edge_add(test_graph, 0, 6, "F"); // (1,7)
+	g_edge_add(test_graph, 6, 7, "G"); // (7,8)
+	g_edge_add(test_graph, 6, 8, "H"); // (7,9)
+
+	SimpleSet set;
+    set_init(&set);
+
+    // Test
+
+	printf("Set with 2/3 additons duplicated should have length 2: ");
+
+	vertex_t nodeToAdd1 = g_vertex_get(test_graph, 0);
+	vertex_t nodeToAdd2 = g_vertex_get(test_graph, 1);
+	vertex_t nodeToAdd3 = g_vertex_get(test_graph, 0);
+
+    set_add(&set, nodeToAdd1);
+    set_add(&set, nodeToAdd2);
+    set_add(&set, nodeToAdd3);
+
+    int setLength = set_length(&set);
+
+    if (setLength == 2){
+    	success_or_failure(1);
+    }
+    else{
+    	success_or_failure(0);
+    }    
+
+
+    // Cleanup
+    set_destroy(&set);
+   	g_free_alt(test_graph, false);
+
 }
 
 // Unit tests for Queue
 
-static void test_queue_empty(){
+MU_TEST(test_queue_empty) {
+    printf("Testing that empty queues have length 0\n");
+    queue_list_t test_queue = q_init();
+    int qsize = q_num_elements(test_queue);
+
+    mu_assert_int_eq(0, qsize);
+
+    q_free(test_queue);
 
 }
 
-static void test_queue_single_item(){
+MU_TEST(test_queue_single_item) {
 
-}
+	//Init 
+	graph_t test_graph = g_init();						//create Test Graph
+	g_vertex_add(test_graph, "1"); //0
+	g_vertex_add(test_graph, "2"); //1
+	g_vertex_add(test_graph, "3"); //2
+	g_vertex_add(test_graph, "4"); //3
+	g_vertex_add(test_graph, "5"); //4
+	g_vertex_add(test_graph, "6"); //5
+	g_vertex_add(test_graph, "7"); //6
+	g_vertex_add(test_graph, "8"); //7
+	g_vertex_add(test_graph, "9"); //8
 
-static void test_queue_enqueue(){
+	g_edge_add(test_graph, 0, 1, "A"); // (1,2)
+	g_edge_add(test_graph, 1, 2, "B"); // (2,3)
+	g_edge_add(test_graph, 1, 3, "C"); // (2,4)
+	g_edge_add(test_graph, 0, 4, "D"); // (1,5)
+	g_edge_add(test_graph, 4, 5, "E"); // (5,6)
+	g_edge_add(test_graph, 0, 6, "F"); // (1,7)
+	g_edge_add(test_graph, 6, 7, "G"); // (7,8)
+	g_edge_add(test_graph, 6, 8, "H"); // (7,9)
 
-}
+    queue_list_t test_queue = q_init(); 				// Create queue
+    vertex_t nodeToAdd = g_vertex_get(test_graph, 0);
 
-static void test_queue_pop(){
+    q_push(test_queue, nodeToAdd); 						//Add item to queue
+    int qsize = q_num_elements(test_queue); 			//Get size of queue
+
+    printf("Testing that single item queues have length 1\n");
+
+    mu_assert_int_eq(1, qsize); 						//run test
+
+    //Cleanup
+    q_free(test_queue); 
 
 }
 
@@ -465,6 +625,17 @@ static void test_bfs_cycle(){
 
 }
 
+
+// Test infrastructure
+
+
+MU_TEST_SUITE(test_suite) {
+	printf("\n\n==== Test for Queues ====\n");
+
+    MU_RUN_TEST(test_queue_empty);
+    MU_RUN_TEST(test_queue_single_item);
+}
+
 //Main Function
 int main(){
 	test_empty_graph();
@@ -475,4 +646,11 @@ int main(){
 	test_get_adjacent_nodes();
 	// Set tests
 	test_set_empty();
+	test_set_single_item();
+	test_set_no_duplicates_added();
+
+	MU_RUN_SUITE(test_suite);
+    MU_REPORT();
+    printf("Number failed tests: %d\n", minunit_fail);
+    return minunit_fail;
 }
