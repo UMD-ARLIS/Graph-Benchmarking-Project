f4f02d1e054e78de11bfcc290a63379fe5bfbd85
Nandini Ramachandran <nandini.ramachandran@gmail.com>
2023-06-08 10:44:39 -0500

    adding file structure to seq bfs implementation

--- Code/Graph_Problems/BFS/Nandini Ramachandran/bfs_v2/bfs_seq.h
+++ Code/Graph_Problems/BFS/Nandini Ramachandran/bfs_v2/bfs_seq.h
@@ -1,7 +1,6 @@
-// bfs.h
-
 // Define the maximum number of vertices in the graph
 #define NUM_VERTICES 6
+
 
 struct Graph{
     struct Node* head[NUM_VERTICES];
@@ -17,46 +16,29 @@ struct Edge{
     int end;
 };
 
-// Structure to hold BFS data
-struct BFSData {
-    struct Graph* graph;
-    bool* visited;
-    int* queue;
-    int front;
-    int rear;
-    struct str_builder* path;
-};
-
-
-// Function to create a graph
 struct Graph* createGraph(struct Edge edges[], int numEdges){
     struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
-
-    // Sets all the head references to null
+    
+    //sets all the head references to null
     for (int i = 0; i < NUM_VERTICES; i++){
         graph->head[i] = NULL;
     }
-    
-    // Build adjacency list representation of the graph
     for (int i = 0; i < numEdges; i++){
-        // Get start and end vertices
+        //gets start and end vertices
         int start = edges[i].start;
         int end = edges[i].end;
-
+        
         struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
         newNode->end = end;
-
-        // Points the new node to the current head
+        
+        //points the new node to the current head
         newNode->next = graph->head[start];
-        // Points head to the new node
+        //points head to the new node
         graph->head[start] = newNode;
     }
-    
     return graph;
 }
 
 void printGraph(struct Graph* graph);
-
-void* bfsThread(void* arg);
 
 const char* bfs(struct Graph* graph, int startVertex);
---
+++ Code/Graph_Problems/BFS/Nandini Ramachandran/bfs_v2/test.c
@@ -0,0 +1,14 @@
+//System Imports
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <pthread.h>
+
+//Util Imports
+#include "../../../../Utilities/c-utils/src/minunit.h"
+
+//User Imports
+#include "bfs_seq.h"
+
