cd9d6337b777ad4f2313f145b27873c5e19a2806
Nandini Ramachandran <nandini.ramachandran@gmail.com>
2023-06-26 15:11:11 -0700

    Included high level parallel overview of algorithm

--- Code/Graph_Problems/CommunityDetection/Louvian/Parallel/louvian_parallel.c
+++ Code/Graph_Problems/CommunityDetection/Louvian/Parallel/louvian_parallel.c
@@ -1,1 +1,19 @@
 // Code/Graph_Problems/CommunityDetection/Louvian/Parallel/louvian_parallel.c
+
+//
+//  louvain_parallel.c
+//  Louvain Algorithm for Community Detection
+//
+//  Created by Nandini Ramachandran on 6/15/23
+//  References: https://arxiv.org/pdf/0803.0476.pdf
+
+// Implementation High Level Overview for Parallel Processing of Louvian Algorithm 
+/*
+1. Divide the Nodes: Split the nodes of the graph into multiple subsets, assigning each subset to a different thread. The division can be done based on node indices or any other suitable criterion.
+2. Thread Synchronization: Use appropriate synchronization mechanisms, such as mutexes or barriers, to ensure proper synchronization between threads. This is necessary to avoid race conditions and ensure data consistency.
+3. Parallel Iterations: Each thread independently performs iterations of the Louvain algorithm on its assigned subset of nodes. Within each iteration, the thread processes its nodes, calculates modularity changes, and updates the node's community assignment based on the maximum gain.
+4. Thread Communication: At the end of each iteration, threads need to communicate to exchange information about community assignments. This information is necessary for the next iteration as it affects the modularity calculations and community updates. Thread synchronization mechanisms, like barriers, can be used to facilitate this communication.
+5. Termination Condition: After each iteration, threads check for improvements in modularity. If any thread detects an improvement, the algorithm continues with the next iteration. Otherwise, the algorithm terminates.
+6. Finalization: Once the algorithm terminates, the resulting community assignments can be collected and processed as required. This may involve merging communities, post-processing steps, or further analysis.
+7. Thread Cleanup: Proper cleanup should be performed to release any resources and synchronization mechanisms used by the threads
+*/
--- Code/Graph_Problems/CommunityDetection/Louvian/Sequential/README.md
+++ Code/Graph_Problems/CommunityDetection/Louvian/Sequential/README.md
@@ -3,6 +3,22 @@
 ## Description
 Basic sequential implementation of Louvian Algorithm for Community Detection on a graph. Essentially, the algorithm detects communities/clusters within a graph based on a modularity score, or how connected nodes are to one another, in order to dissect a graph into multiple communities.
 
+#### High Level Overview
+1. Initialization:
+    Each node in the graph is assigned to its own community.
+    Compute the modularity of the initial partition.
+2. Repeat until no improvement in modularity is possible:
+    a. For each node:
+        Compute the change in modularity if the node is moved to each of its neighboring communities.
+        Select the community that yields the maximum increase in modularity and move the node to that community.
+    b. If any node changed its community, go back to step 2a.
+3. Post-processing:
+    Each community is represented by a "super-node" in a new graph.
+    The weights of the edges between super-nodes are the sum of the weights of the edges between their respective communities in the original graph.
+    Apply the Louvain algorithm to the new graph to find the communities at the next level.
+4. Repeat steps 2 and 3 until no further improvement in modularity is possible or until a       predefined number of levels is reached.
+5. Return the final set of communities.
+
 ## Known Issues / Future Work
 When lots of nodes and edges are added on a graph (approx 10+) the program stalls and takes a very long time. This may be due to the lengthy process needed to recalculate modularity for every edge or a bug in the code. This needs to be looked into as we hope to implement this algorithm on large datasets.
 
