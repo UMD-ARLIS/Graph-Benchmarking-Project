186796770e5e35bfd4c010fa4ad47777d84aee21
moreinib <135053915+moreinib@users.noreply.github.com>
2023-06-28 12:16:23 -0400

    Update Toy_DataSet.c to include an argument path, and a random seed

--- Code/Dataset_Creation/Morein/Toy_DataSet.c
+++ Code/Dataset_Creation/Morein/Toy_DataSet.c
@@ -2,84 +2,59 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-#define MAX_NODES 100
-#define MAX_EDGES 200
-#define NUM_COMMUNITIES 4
-
+#define MAX_NODES 1000
+#define MAX_EDGES 100000
 
 typedef struct {
-   int node1;
-   int node2;
+    int node1;
+    int node2;
 } Edge;
 
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s <numNodes> <numEdges> <seed>\n", argv[0]);
+        return 1;
+    }
 
-typedef struct {
-   int node;
-   int community;
-} Node;
+    int numNodes = atoi(argv[1]);    // Number of nodes in the graph
+    int numEdges = atoi(argv[2]);    // Number of edges in the graph
+    int seed = atoi(argv[3]);        // Random seed
 
+    // Set the random seed
+    srand(seed);
 
-//modify the values of numNodes and numEdges to generate graphs of different sizes.
+    // Create an array of edges
+    Edge edges[MAX_EDGES];
 
+    // Generate random edges between nodes
+    int numGeneratedEdges = 0;
+    while (numGeneratedEdges < numEdges) {
+        int node1 = rand() % numNodes;
+        int node2 = rand() % numNodes;
 
-int main() {
-   int numNodes = 20;  // Number of nodes in the graph
-   int numEdges = 30;  // Number of edges in the graph
+        // Ensure that the two nodes are distinct and the edge does not already exist
+        if (node1 != node2) {
+            int exists = 0;
+            for (int i = 0; i < numGeneratedEdges; i++) {
+                if ((edges[i].node1 == node1 && edges[i].node2 == node2) ||
+                    (edges[i].node1 == node2 && edges[i].node2 == node1)) {
+                    exists = 1;
+                    break;
+                }
+            }
 
+            if (!exists) {
+                edges[numGeneratedEdges].node1 = node1;
+                edges[numGeneratedEdges].node2 = node2;
+                numGeneratedEdges++;
+            }
+        }
+    }
 
-   // Initialize random number generator
-   srand(time(NULL));
+    // Print the graph in the following format: node1 node2
+    for (int i = 0; i < numEdges; i++) {
+        printf("%d %d\n", edges[i].node1, edges[i].node2);
+    }
 
-
-   // Create an array of nodes
-   Node nodes[MAX_NODES];
-
-
-   // Assign each node to a random community
-   for (int i = 0; i < numNodes; i++) {
-       nodes[i].node = i;
-       nodes[i].community = rand() % NUM_COMMUNITIES;
-   }
-
-
-   // Create an array of edges
-   Edge edges[MAX_EDGES];
-
-
-   // Generate random edges between nodes
-   int edgeCount = 0;
-   while (edgeCount < numEdges) {
-       int node1 = rand() % numNodes;
-       int node2 = rand() % numNodes;
-
-
-       // Ensure that the two nodes are distinct and the edge does not already exist
-       if (node1 != node2) {
-           int exists = 0;
-           for (int i = 0; i < edgeCount; i++) {
-               if ((edges[i].node1 == node1 && edges[i].node2 == node2) ||
-                   (edges[i].node1 == node2 && edges[i].node2 == node1)) {
-                   exists = 1;
-                   break;
-               }
-           }
-
-
-           if (!exists) {
-               edges[edgeCount].node1 = node1;
-               edges[edgeCount].node2 = node2;
-               edgeCount++;
-           }
-       }
-   }
-
-
-   // Print the graph in the following format: node1 node2
-   for (int i = 0; i < numEdges; i++) {
-       printf("%d %d\n", edges[i].node1, edges[i].node2);
-   }
-
-
-   return 0;
+    return 0;
 }
