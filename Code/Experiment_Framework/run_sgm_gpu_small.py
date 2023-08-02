338aaff2e62748d6afea688349cc75787ded86f4
ubuntu user <osullik@umd.edu>
2023-08-02 12:40:49 +0000

    Basics of GPU eval done

---
+++ Code/Experiment_Framework/run_sgm_gpu_small.py
@@ -0,0 +1,40 @@
+import subprocess
+
+def run_commands(commands):
+    output = ""
+    for command in commands:
+        print("Running", command)
+        try:
+            result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
+            output += f"Command: {command}\nOutput:\n{result}\n"
+            print("Completed Successfully\n")
+        except subprocess.CalledProcessError as e:
+            output += f"Command: {command}\nError:\n{e.output}\n"
+            print("Failed")
+
+    return output
+
+if __name__ == "__main__":
+    commands = [
+        #Run the timing commands:
+        "./../Graph_Problems/GraphMatching/gunrock/examples/sm/bin/test_sm_12.2_x86_64 --pattern-graph-type=market --pattern-graph-file=../../Data/subgraph_matching/SMALL_SUB_target.mtx --undirected=0 --pattern-undirected=0 --num-runs=1 --graph-type=market --graph-file=../../Data/subgraph_matching/SMALL_target.mtx",
+	"./../Graph_Problems/GraphMatching/gunrock/examples/sm/bin/test_sm_12.2_x86_64 --pattern-graph-type=market --pattern-graph-file=../../Data/subgraph_matching/SMALL_SUB4_target.mtx --undirected=0 --pattern-undirected=0 --num-runs=1 --graph-type=market --graph-file=../../Data/subgraph_matching/SMALL_target.mtx",
+	"./../Graph_Problems/GraphMatching/gunrock/examples/sm/bin/test_sm_12.2_x86_64 --pattern-graph-type=market --pattern-graph-file=../../Data/subgraph_matching/MEDIUM_SUB_email-Enron.mtx --undirected=0 --pattern-undirected=0 --num-runs=1 --graph-type=market --graph-file=../../Data/subgraph_matching/MEDIUM_email-enron.mtx",
+	"./../Graph_Problems/GraphMatching/gunrock/examples/sm/bin/test_sm_12.2_x86_64 --pattern-graph-type=market --pattern-graph-file=../../Data/subgraph_matching/MEDIUM_SUB4_email-Enron.mtx --undirected=0 --pattern-undirected=0 --num-runs=1 --graph-type=market --graph-file=../../Data/subgraph_matching/MEDIUM_email-enron.mtx"
+
+        #"./../Graph_Problems/GraphMatching/vf3lib/bin/vf3p -v ./../../Data/subgraph_matching/SMALL_A.01/SMALL_SUB_4_pattern.grf ./../../Data/subgraph_matching/SMALL_A.01/SMALL_target.grf a 1 -t 2",
+        #"./../Graph_Problems/GraphMatching/vf3lib/bin/vf3p -v ./../../Data/subgraph_matching/SMALL_A.01/SMALL_SUB_3_pattern.grf ./../../Data/subgraph_matching/SMALL_A.01/SMALL_target.grf a 1 -t 10",
+        #"./../Graph_Problems/GraphMatching/vf3lib/bin/vf3p -v ./../../Data/subgraph_matching/SMALL_A.01/SMALL_SUB_4_pattern.grf ./../../Data/subgraph_matching/SMALL_A.01/SMALL_target.grf a 1 -t 10",
+        #Run the Memory Commands
+        #"bash sgm_memory_small.sh"
+    ]
+
+    # Run commands and collect results
+    result = run_commands(commands)
+
+    # Output results to a file
+    output_file_path = "small_gpu_results.txt"
+    with open(output_file_path, "w") as file:
+        file.write(result)
+
+    print(f"Results have been saved to {output_file_path}.")
