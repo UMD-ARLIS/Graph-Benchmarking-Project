2865580d7d15866520eca2f0faf802d9a2f12723
Nandini Ramachandran <nandini.ramachandran@gmail.com>
2023-11-14 13:31:31 -0500

    working gpu_experiment script

--- Code/Experiment_Framework/experiment/gpu_experiment.py
+++ Code/Experiment_Framework/experiment/gpu_experiment.py
@@ -1,25 +1,50 @@
 import os
-from experiment.experiment import Experiment
+from experiment import Experiment
 import subprocess
 
+# add functionality to activate virtual environment
 class GPUExperiment(Experiment):
     def __init__(self, name: str, num_workers: int, algorithm: str, graph_file: str):
         super().__init__(name, algorithm, num_workers)
         self._graph_file = graph_file
 
-    def compile_code(self):
-        make_file_path = "../make.py"
+    def compile_code(self, nvcc_path, nvcc_version):
+        make_file_path = "/home/test-gunrock/Graph-Benchmarking-Project/Code/Experiment_Framework/make.py"
 
-        if os.path.isfile(self._graph_file):
-            self.load_graph(self._graph_file)
+        """ if os.path.isfile(self._graph_file):
+            self.load_graph(self._graph_file) """
 
-        self.dump_experiment_metadata()
+        # Compile the code using make.py script with parameters
+        subprocess.run(f"sudo python {make_file_path} --nvcc {nvcc_path} --nvcc_version {nvcc_version}", shell=True)
 
-        # Assuming you want to run make.py for compilation
-        subprocess.run(f"python {make_file_path}", shell=True)
+    def run_command(self, nvcc_version, dataset_path, num_workers, algorithm):
+        louvain_executable = f"./examples/{algorithm}/bin/test_{algorithm}_{nvcc_version}_x86_64"
+        command = f"{louvain_executable} --omp-threads={num_workers} --iter-stats --pass-stats " \
+                  f"--advance-mode=ALL_EDGES --unify-segments=true --validation=each " \
+                  f"--num-runs=10 --graph-type=market --graph-file=dataset/small/chesapeake.mtx"
+        # make sure to update the dataset path to come from github directly
+
+        # Store the current directory
+        current_directory = os.getcwd()
+
+        try:
+            # Change directory to the desired location
+            os.chdir("/home/test-gunrock/Graph-Benchmarking-Project/Code/Graph_Problems/CommunityDetection/Louvian/GPU/gunrock_louvain")
+
+            subprocess.run(command, shell=True)
+
+        finally:
+            # Change back to the original directory
+            os.chdir(current_directory)
+
+    def run_experiment(self, nvcc_path, nvcc_version):
+        self.compile_code(nvcc_path, nvcc_version)
+        self.run_command(nvcc_version, self._graph_file, self._num_workers, self._algorithm)
 
 if __name__ == "__main__":
     # Create an instance of GPUExperiment
-    gpu_exp = GPUExperiment(name="gpu001", algorithm="Louvain", num_workers=1, graph_file="Data/community_detection/SMALL_chesapeake.mtx")
-    gpu_exp.compile_code()
-    # execute rest of processes
+    gpu_exp = GPUExperiment(name="gpu001", algorithm="louvain", num_workers=32, graph_file="/home/test-gunrock/Graph-Benchmarking-Project/Data/community_detection/SMALL_chesapeake.mtx")
+    nvcc_path = "/usr/local/cuda-12.2/bin/nvcc"  # Set your NVCC path
+    nvcc_version = "12.2"  # Set your NVCC version
+
+    gpu_exp.run_experiment(nvcc_path, nvcc_version)
--- Code/Experiment_Framework/make.py
+++ Code/Experiment_Framework/make.py
@@ -1,29 +1,49 @@
 import subprocess
 import os
+import sys
+import argparse
 
+def modify_makefile_nvcc_values(NVCC, NVCC_VERSION):
+        with open('/home/test-gunrock/Graph-Benchmarking-Project/Code/Graph_Problems/CommunityDetection/Louvian/GPU/gunrock_louvain/examples/BaseMakefile.mk', 'r') as file:
+            filedata = file.read()
 
+    # Replace NVCC and NVCC_VERSION values
+        filedata = filedata.replace('NVCC = "/usr/local/cuda-12.2/bin/nvcc"', f'NVCC = "{NVCC}"')
+        filedata = filedata.replace('NVCC_VERSION = 12.2', f'NVCC_VERSION = {NVCC_VERSION}')
 
-
-def change_directory_and_permissions():
-    # Change permissions in the build directory
-    subprocess.run("ps -p $$")
-    subprocess.run('chmod -R u+w ../Graph_Problems/CommunityDetection/Louvian/GPU/gunrock_louvain/build', shell=True)
+        with open('/home/test-gunrock/Graph-Benchmarking-Project/Code/Graph_Problems/CommunityDetection/Louvian/GPU/gunrock_louvain/examples/BaseMakefile.mk', 'w') as file:
+            file.write(filedata)
 
 # Function to build using CMake
 def cmake_build():
-    subprocess.run('cd ../Graph_Problems/CommunityDetection/Louvian/GPU/gunrock_louvain && mkdir -p build && cd build && cmake ..', shell=True)
+    subprocess.run('cd /home/test-gunrock/Graph-Benchmarking-Project/Code/Graph_Problems/CommunityDetection/Louvian/GPU/gunrock_louvain && mkdir -p build && cd build && cmake -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-12.2 ..', shell=True, executable="/usr/bin/bash")
+    print(os.getcwd())
 
 # Function to make louvain tests
 def louvain_test():
-    subprocess.run('cd ../Graph_Problems/CommunityDetection/Louvian/GPU/gunrock_louvain/examples/louvain && make', shell=True)
+    subprocess.run('cd /home/test-gunrock/Graph-Benchmarking-Project/Code/Graph_Problems/CommunityDetection/Louvian/GPU/gunrock_louvain/examples/louvain && make clean && make', shell=True, executable="/usr/bin/bash")
 
 # Function to simulate deployment
 def deploy():
     print('Branch: gunrock_integration.')
     print('Pipeline finished.')
 
-# Execute the functions
-change_directory_and_permissions()
-cmake_build()
-louvain_test()
-deploy()
+
+if len(sys.argv) > 1:
+    NVCC = sys.argv[1]
+    NVCC_VERSION = sys.argv[2]
+
+def main():
+    parser = argparse.ArgumentParser(description="Build script for project")
+    parser.add_argument("--nvcc", help="Path to nvcc executable", default="/usr/local/cuda-12.2/bin/nvcc")
+    parser.add_argument("--nvcc_version", help="NVCC version", default="12.2")
+    
+    args = parser.parse_args()
+
+    modify_makefile_nvcc_values(args.nvcc, args.nvcc_version)
+    cmake_build()
+    louvain_test()
+    deploy()
+
+if __name__ == "__main__":
+    main()
